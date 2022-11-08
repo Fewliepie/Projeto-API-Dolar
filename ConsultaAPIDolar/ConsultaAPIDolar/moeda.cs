6577ba395e3fccd0ace150befd29d9d54bf03a08
Felipe Araujo <felipearanas35@gmail.com>
2022-11-07 23:14:17 -0300

    Projeto de consumo de API

---
+++ ConsultaAPIDolar/ConsultaAPIDolar/moeda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaAPIDolar
+{
+    public class moeda
+    {
+        public string simbolo { get; set; }
+        public string nomeFormatado { get; set; }
+        public string tipoMoeda { get; set; }
+    }
+}
---
+++ ConsultaAPIDolarTESTE/ConsultaAPIDolarTESTE/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Moedas, CotacaoDolarDia, CotacaoDolarPeriodo, CotacaoMoedaDia, CotacaoMoedaPeriodo");
+var recurso = Console.ReadLine();
+
+
+
+
+
+
+
+Console.WriteLine("Data da cotação - informar no padrão 'MM-DD-AAAA'");
+var parametro = Console.ReadLine();
+//var endereco = $@"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/{recurso}?$format=json&";
+var endereco = $@"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/{recurso}(dataCotacao=@dataCotacao)?@dataCotacao='{parametro}'&$format=json";
+
+var client  = new HttpClient();
+try
+{
+	HttpResponseMessage? response = await client.GetAsync(endereco);
+	response.EnsureSuccessStatusCode();
+	string responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
+   // List<moeda> Moedas = JsonSerializer.Deserialize<List<moeda>>(responseBody);
+
+
+   // foreach (var moeda in Moedas)
+	//{
+	//	Console.WriteLine(moeda.simbolo);
+	//	Console.WriteLine(moeda.nomeFormatado);
+	//	Console.WriteLine(moeda.tipoMoeda);
+	//}
+}
+
+catch (Exception)
+{
+
+	throw;
+}
+
